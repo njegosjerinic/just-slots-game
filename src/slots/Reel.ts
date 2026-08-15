c0c11e199559c98dd7deb9b22c942fe26b803367
Njegos Jerinic <95654934+njegosjerinic@users.noreply.github.com>
2026-08-15 23:38:16 +0200

    Implement horizontal reel animation

--- src/slots/Reel.ts
+++ src/slots/Reel.ts
@@ -1,75 +1,120 @@
-import * as PIXI from 'pixi.js';
-import { AssetLoader } from '../utils/AssetLoader';
+import * as PIXI from "pixi.js";
+import { AssetLoader } from "../utils/AssetLoader";
 
 const SYMBOL_TEXTURES = [
-    'symbol1.png',
-    'symbol2.png',
-    'symbol3.png',
-    'symbol4.png',
-    'symbol5.png',
+  "symbol1.png",
+  "symbol2.png",
+  "symbol3.png",
+  "symbol4.png",
+  "symbol5.png",
 ];
 
 const SPIN_SPEED = 50; // Pixels per frame
 const SLOWDOWN_RATE = 0.95; // Rate at which the reel slows down
 
 export class Reel {
-    public container: PIXI.Container;
-    private symbols: PIXI.Sprite[];
-    private symbolSize: number;
-    private symbolCount: number;
-    private speed: number = 0;
-    private isSpinning: boolean = false;
+  public container: PIXI.Container;
+  private symbols: PIXI.Sprite[];
+  private symbolSize: number;
+  private symbolCount: number;
+  private speed: number = 0;
+  private isSpinning: boolean = false;
 
-    constructor(symbolCount: number, symbolSize: number) {
-        this.container = new PIXI.Container();
-        this.symbols = [];
-        this.symbolSize = symbolSize;
-        this.symbolCount = symbolCount;
+  constructor(symbolCount: number, symbolSize: number) {
+    this.container = new PIXI.Container();
+    this.symbols = [];
+    this.symbolSize = symbolSize;
+    this.symbolCount = symbolCount;
 
-        this.createSymbols();
+    this.createSymbols();
+
+    const mask = new PIXI.Graphics();
+
+    mask.beginFill(0xffffff);
+    mask.drawRect(0, 0, this.symbolCount * this.symbolSize, this.symbolSize);
+    mask.endFill();
+
+    this.container.addChild(mask);
+    this.container.mask = mask;
+  }
+
+  private createSymbols(): void {
+    // Create symbols for the reel, arranged horizontally
+    for (let i = 0; i <= this.symbolCount; i++) {
+      const symbol = this.createRandomSymbol();
+      symbol.x = i * this.symbolSize - 150;
+      symbol.anchor.y = 0.5;
+      symbol.y = this.symbolSize / 2;
+
+      this.symbols.push(symbol);
+      this.container.addChild(symbol);
+    }
+  }
+
+  private createRandomSymbol(): PIXI.Sprite {
+    // TODO:Get a random symbol texture
+    const randomSymbol = AssetLoader.getTexture(
+      SYMBOL_TEXTURES[Math.floor(Math.random() * SYMBOL_TEXTURES.length)],
+    );
+
+    // TODO:Create a sprite with the texture
+    const sprite = new PIXI.Sprite(randomSymbol);
+
+    sprite.width = 140;
+
+    sprite.height = 140;
+
+    return sprite;
+  }
+
+  public update(delta: number): void {
+    if (!this.isSpinning && this.speed === 0) return;
+
+    // TODO:Move symbols horizontally
+
+    const visibleWidth = this.symbolCount * this.symbolSize; // 900
+    const trackWidth = (this.symbolCount + 1) * this.symbolSize; // 1050
+
+    for (const symbol of this.symbols) {
+      symbol.x += this.speed * delta;
+
+      if (symbol.x >= visibleWidth) {
+        symbol.x -= trackWidth;
+      }
     }
 
-    private createSymbols(): void {
-        // Create symbols for the reel, arranged horizontally
+    // If we're stopping, slow down the reel
+    if (!this.isSpinning && this.speed > 0) {
+      this.speed *= SLOWDOWN_RATE;
+
+      // If speed is very low, stop completely and snap to grid
+      if (this.speed < 0.5) {
+        this.speed = 0;
+        this.snapToGrid();
+      }
     }
+  }
 
-    private createRandomSymbol(): PIXI.Sprite {
-        // TODO:Get a random symbol texture
+  private snapToGrid(): void {
+    const visibleWidth = this.symbolCount * this.symbolSize; // 900
+    const trackWidth = (this.symbolCount + 1) * this.symbolSize; // 1050
 
-        // TODO:Create a sprite with the texture
+    for (const symbol of this.symbols) {
+      symbol.x = Math.round(symbol.x / this.symbolSize) * this.symbolSize;
 
-        return new PIXI.Sprite();
+      if (symbol.x >= visibleWidth) {
+        symbol.x -= trackWidth;
+      }
     }
+  }
 
-    public update(delta: number): void {
-        if (!this.isSpinning && this.speed === 0) return;
+  public startSpin(): void {
+    this.isSpinning = true;
+    this.speed = SPIN_SPEED;
+  }
 
-        // TODO:Move symbols horizontally
-
-        // If we're stopping, slow down the reel
-        if (!this.isSpinning && this.speed > 0) {
-            this.speed *= SLOWDOWN_RATE;
-
-            // If speed is very low, stop completely and snap to grid
-            if (this.speed < 0.5) {
-                this.speed = 0;
-                this.snapToGrid();
-            }
-        }
-    }
-
-    private snapToGrid(): void {
-        // TODO: Snap symbols to horizontal grid positions
-
-    }
-
-    public startSpin(): void {
-        this.isSpinning = true;
-        this.speed = SPIN_SPEED;
-    }
-
-    public stopSpin(): void {
-        this.isSpinning = false;
-        // The reel will gradually slow down in the update method
-    }
+  public stopSpin(): void {
+    this.isSpinning = false;
+    // The reel will gradually slow down in the update method
+  }
 }
